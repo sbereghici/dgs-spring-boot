b7ad222a79b48b27b03fe68c5f8f5cabb5b36ca9
Stefan Bereghici <sbereghici@gmail.com>
2022-07-05 10:11:07 +0300

    Init

---
+++ src/main/java/com/example/dgsspringboot/DgsSpringBootApplication.java
@@ -0,0 +1,13 @@
+package com.example.dgsspringboot;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DgsSpringBootApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(DgsSpringBootApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/example/dgsspringboot/DgsSpringBootApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.dgsspringboot;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DgsSpringBootApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
